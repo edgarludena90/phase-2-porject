c6946e2ea28fedb474405ccdfffc6759d7e91131
edgar ludena <edgar.ludena90@gmail.com>
2022-12-06 12:08:55 -0500

    12:10 update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,27 @@
 import React from 'react'
 
 function Header() {
-  return(
-  <header>
-    <nav>
-      <h1 className='branding'>
-        <span className='logo'>{'//'}</span>
-        Project Showcase
-      </h1>
-      <div className='navigation'>
-        <a className='button' href='/projects'>
-          All Projects
-        </a>
-        <a className='button' href='/projects/new'>
-          Add Project
-        </a>
-        <button></button>
-      </div>
-    </nav>
-  </header>
+  return (
+    <header>
+      <nav>
+        <h1 className='branding'>
+          <span className='logo'>{'//'}</span>
+          ANIME WORLD
+        </h1>
+        <div className='navigation'>
+          <a className='button' href='/projects'>
+            ANIME BIO
+          </a>
+          <a className='button' href='/projects/new'>
+            ANIME HIGHLIGHTS
+          </a>
+          <a className='button' href='/projects/new'>
+            ADD YOUR ANIME
+          </a>
+          
+        </div>
+      </nav>
+    </header>
   )
 }
 
